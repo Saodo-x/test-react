2e5303974df200d88c87c5bc09317b57bdc1432c
unknown <xusw@links.sz>
2022-07-11 17:13:30 +0800

    test2-react1

--- JS/test2-r5.js
+++ JS/test2-r5.js
@@ -6,14 +6,15 @@ class Reservation extends React.Component {
         this.state = {
             isGoing: true,
             numberOfGuests: 2,
-            inputTxt: '',
+            inputTxt: 'sss',
             texts: '哈哈哈',
             value: [
                 {value1: 'grapefruit', txt: '葡萄柚'},
                 {value1: 'lime', txt: '酸橙'},
                 {value1: 'coconut', txt: '椰子'},
                 {value1: 'mango', txt: '芒果'},
-            ]
+            ],
+            valueReal: 'coconut'
         }
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
@@ -25,6 +26,12 @@ class Reservation extends React.Component {
         if (name === 'isGoing'){
             const value = target.checked
             this.setState({[name]: value})
+        }
+        else if (name === 'value'){
+            const value = target.value
+            // console.log(value)
+            // console.log(target)
+            this.setState({valueReal: value})
         }else {
             const value = target.value
             this.setState({[name]: value})
@@ -58,17 +65,17 @@ class Reservation extends React.Component {
                 <br />
                 <label>
                     名字:
-                    <input type="text" name='txt' value={this.state.inputTxt} onChange={this.handleChange} />
+                    <input type="text" name='inputTxt' value={this.state.inputTxt} onChange={this.handleChange} />
                 </label>
                 <br />
                 <label>
                     文章:
-                    <textarea value={this.state.texts} name='text' onChange={this.handleChange} />
+                    <textarea value={this.state.texts} name='texts' onChange={this.handleChange} />
                 </label>
                 <br />
                 <label>
                     选择你喜欢的风味:
-                    <NumList value={this.state.value} onChange={this.handleChange}/>
+                    <NumList value={this.state.value} onChange={this.handleChange} valuereal={this.state.valueReal}/>
                 </label>
                 <br />
                 <input type="submit" value="提交" />
@@ -82,11 +89,13 @@ function ListItem (props){
 }
 
 function NumList (props){
-    const value = props.value
+    const val = props.value
+    const real = props.valuereal
+    // console.log(real)
     return (
-        <select onChange={props.onChange}>
-            {value.map((item,index)=>
-                <ListItem key={index} value={item.value} txt={item.txt}/>
+        <select onChange={props.onChange} name='value' value={real}>
+            {val.map((item,index)=>
+                <ListItem key={index} value={item.value1} txt={item.txt}/>
             )}
         </select>
     )
